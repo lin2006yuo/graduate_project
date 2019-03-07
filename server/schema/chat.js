f062acdd2098d5c59bb2be9886baacde9617b3c7
lin2006yuo <547677504@qq.com>
2019-03-07 22:00:48 +0800

    h3

--- server/schema/chat.js
+++ server/schema/chat.js
@@ -2,14 +2,23 @@ const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
 const chatSchema = new Schema({
-    from: { type: String, required: true },
-    to: { type: String, required: true },
+    chatlist: [
+        {
+            from: { type: String, required: true },
+            to: { type: String, required: true },
+            from: { type: String, required: true },
+            read: { type: Boolean, default: false },
+            create_time: { type: Number },
+            content: { type: String, required: true },
+        }
+    ],
+    chator: {
+      creator: { type: mongoose.Types.ObjectId, ref: 'studentModel' },
+      target: { type: mongoose.Types.ObjectId, ref: 'companyModel' }
+    },
     // fromModel: { type: String, required: true, enum: ['studentModel', 'companyModel'] },
     // toModel: { type: String, required: true, enum: ['studentModel', 'companyModel'] },
     chat_id: { type: String, required: true },
-    content: { type: String, required: true },
-    read: { type: Boolean, default: false },
-    create_time: { type: Number },
 })
 
 module.exports = chatSchema
